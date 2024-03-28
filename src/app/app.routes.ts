5b6b20681f21e5f0f7c00cd21055747e9faff183
MoisesST <moises.dev13@gmail.com>
2024-03-28 15:09:20 -0300

    bug fix menu duplicado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
 import { CreateTaskComponent } from './components/pages/tasks/create-task/create-task.component';
+import { ListTaskComponent } from './components/pages/tasks/list-task/list-task.component';
 
 export const routes: Routes = [
-  { path: 'home', title: 'Home', component: AppComponent },
+  { path: 'home', title: 'Home', component: ListTaskComponent },
   { path: 'create-task', title: 'Create Task', component: CreateTaskComponent },
 ];
