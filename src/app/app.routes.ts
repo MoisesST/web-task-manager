2df28b5b0582227f01494ebaab12e223a2c8ba03
MoisesST <moises.dev13@gmail.com>
2024-03-28 14:50:39 -0300

    page create task implementada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
+import { CreateTaskComponent } from './components/pages/tasks/create-task/create-task.component';
+
 export const routes: Routes = [
   { path: 'home', title: 'Home', component: AppComponent },
+  { path: 'create-task', title: 'Create Task', component: CreateTaskComponent },
 ];
--- src/app/components/template/main/main.component.ts
+++ src/app/components/template/main/main.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { TitleComponent } from '../../elements/title/title.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [RouterOutlet],
   template: `
     <main>
-      <div class="w-full md:w-[1000px] mx-auto">
-        <app-title name="Home - Test" />
+      <div
+        class="
+          flex flex-col w-11/12 sm:w-10/12 md:w-8/12 lg:w-7/12 xl:w-6/12 mx-auto
+        "
+      >
+        <router-outlet />
       </div>
     </main>
   `,
