761d74e278764be0dc5a604d9a5a32c9b5105372
MoisesST <moises.dev13@gmail.com>
2024-05-22 14:01:57 -0300

    pipe implementado

--- src/app/components/elements/title/title.component.ts
+++ src/app/components/elements/title/title.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { CapitalizePipe } from '../../../pipes/capitalize.pipe';
 
 @Component({
   selector: 'app-title',
   standalone: true,
+  imports: [CapitalizePipe],
   template: `
     <h1
       [class]="style"
-      class="text-2xl md:text-4xl font-medium">{{ name }}
+      class="text-2xl md:text-4xl font-medium">{{ name | capitalize }}
     </h1>
   `,
 })
