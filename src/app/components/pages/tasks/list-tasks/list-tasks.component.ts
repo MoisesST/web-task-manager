b7b6e43fed793c48224f8f63bee5c712bcb8b571
eliezerbuczak <eliezerbuczak2016@gmail.com>
2024-05-26 17:30:09 -0300

    feat: atividade 15

--- src/app/components/pages/tasks/list-tasks/list-tasks.component.ts
+++ src/app/components/pages/tasks/list-tasks/list-tasks.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 import { TaskComponent } from '../../../elements/task/task.component';
 import { NoTasksComponent } from '../../../template/no-tasks/no-tasks.component';
 import { TitleComponent } from '../../../elements/title/title.component';
+import {FormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-list-task',
   standalone: true,
-  imports: [TitleComponent, TaskComponent, NoTasksComponent],
+  imports: [TitleComponent, TaskComponent, NoTasksComponent, FormsModule, NgForOf, NgIf],
   templateUrl: './list-tasks.component.html',
 })
 export class ListTasksComponent {
@@ -18,6 +20,35 @@ export class ListTasksComponent {
     const response = await fetch('http://localhost:8000/categories');
     const data = await response.json();
     return data;
+  }
+
+  //create function to post category
+
+  categoryName: string = '';
+
+  async addCategory() {
+    if (this.categoryName.trim()) {
+      const newCategory = { name: this.categoryName };
+      const response = await fetch('http://localhost:8000/categories', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newCategory),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        this.getCategories().then((data: any) => {
+          this.categories = data;
+        }).catch((error: any) => {
+          console.error(error);
+        })
+        this.categoryName = '';
+      } else {
+        console.error('Erro ao adicionar categoria:', response.statusText);
+      }
+    }
   }
 
   ngOnInit() {
